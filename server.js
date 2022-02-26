7258ee0943fbb092eeb835b076079950d734bdc1
Kerry Long <victory173451@gmail.com>
2022-02-26 17:02:07 -0500

    fixed load and added delete functionality

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -14,66 +16,114 @@ const PORT = process.env.PORT || 3001;
 const uuid = require('./helpers/uuid');
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 
 app.get('/api/notes', (req, res) => {
-    console.log(notes);
-    res.json(notes);
-})
+
+  let readNotes = JSON.parse(fs.readFileSync('./data/notes.json'));
+  res.json(readNotes);
+});
+
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.post('/api/notes', (req, res) => {
 
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
 
-    // If all the required properties are present
-    if (title && text) {
-        // Variable for the object we will save
-        const newNote = {
-            title,
-            text,
-            note_id: uuid(),
-        };
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
 
 
-       // Obtain existing reviews
+    // Obtain existing notes
     fs.readFile('./data/notes.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNotes = JSON.parse(data);
-  
-          // Add a new review
-          parsedNotes.push(newNote);
-  
-          // Write updated reviews back to the file
-          fs.writeFile(
-            './data/notes.json',
-            JSON.stringify(parsedNotes, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated reviews!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.json(response);
-    } else {
-      res.json('Error in posting review');
+      if (err) {
+        console.error(err);
+      }
+      else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFileSync('./data/notes.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+        //res.json(parsedNotes);
+      }
+    });
+
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(newNote);
+  }
+  else {
+    res.json('Error in posting review');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+
+
+  const noteId = req.params.id;
+
+  // Obtain existing notes
+  fs.readFile('./data/notes.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      // Convert string into JSON object
+      const parsedNotes = JSON.parse(data);
+      //console.log("******Deleting NoteID************** " + noteId);
+      newParsedNotes = parsedNotes.filter(x => x.note_id !== noteId);
+      //console.log(newParsedNotes);
+
+      
+      //parsedNotes.push(newNote);
+
+      // Write updated notes back to the file
+      fs.writeFileSync('./data/notes.json',
+        JSON.stringify(newParsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+      );
+      res.json(parsedNotes);
     }
   });
-  
+
+});
+
+
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
